725b05178b4a6d67c3e95bae4d9fc1f8d5bfe139
luispfcanales <luispfcanales@gmail.com>
2021-05-18 18:51:47 -0500

    first upload proyect circuitos

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,54 @@
 <script>
-	export let name;
+	import Card from './components/Card.svelte'
 </script>
 
 <main>
-	<h1>hi! {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Unamad</h1>
+<div>
+	<Card urlImage="/licuadora-1.png"/>
+	<Card urlImage="/lavadora-1.png"/>
+	<Card urlImage="/plancha-1.jpg"/>
+	<Card urlImage="/licuadora-1.png"/>
+	<Card urlImage="/lavadora-1.png"/>
+</div>
+<h3>luis angel pfuño canales</h3>
+<p>My repository <a href="https://github.com/luis16121013">github</a> </p>
+
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+main {
+text-align: center;
+padding: .2em;
+max-width: 400px;
+margin: 0 auto;
+border:1px solid orange;
+}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+h1 {
+color: #ff3e00;
+text-transform: uppercase;
+font-size: 4em;
+font-weight: 100;
+}
+h3 {
+color: #ff3e00;
+}
+div {
+display:flex;
+border:1px solid red;
+flex-direction:column;
+flex-wrap:wrap;
+align-items:center;
+}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+@media (min-width: 640px) {
+main {
+	max-width: none;
+}
+div {
+	flex-direction:row;
+	justify-content:center;
+}
+}
 </style>
---
+++ src/components/Card.svelte
@@ -0,0 +1,42 @@
+<script>
+	export let urlImage=""
+</script>
+<div class="container-card">
+	hola
+	<div class="content-card">
+		<div>
+			<img src={urlImage} alt="img">
+		</div>
+		<div>
+			description
+		</div>
+	</div>
+</div>
+
+<style>
+.container-card {
+min-width:320px;
+min-height:200px;
+border:1px solid peru;
+margin:4px;
+border-radius:5px;
+}
+.content-card {
+	box-sizing:border-box;
+	display:flex;
+	width:100%;
+	height:200px;
+	border:1px solid cyan;
+}
+
+img {
+width:130px;
+}
+
+@media (min-width: 640px) {
+.container-card {
+	max-width:340px;
+}
+.content-card {
+}
+</style>
